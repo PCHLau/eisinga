1ab95653c7593329cd6b97f5c0cf5a58b67f503d
patrick <patrick.zk.lau@gmail.com>
2024-12-07 01:36:56 +0100

    now able to generate rudimentary clock face showing sun and moon rise and set times

--- risetest.py
+++ risetest.py
@@ -4,6 +4,31 @@ import datetime
 import matplotlib.pyplot as plt
 import math
 
+def clock(tt):
+    time = tt.utc_datetime()[0]
+    if time.hour > 12:
+        a = time.hour - 12
+    else:
+        a = time.hour
+
+    b =  a + time.minute/60 + time.second/3600
+
+    angle = -((b/12 * math.pi)*2) + math.pi/2
+
+    x = math.cos(angle)*0.53
+    y = math.sin(angle)*0.53
+
+    img = plt.imread('clock.png')
+    fig, ax = plt.subplots()
+    ax.imshow(img, extent=[-1, 1, -1, 1])
+    plt.axis('off')
+    plt.arrow(-x, -y, 2*x, 2*y, width = 0.03, length_includes_head=True,
+            head_width=0.12, overhang = 0.2, color = 'k', )
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
+
+    plt.show()
+
 ts = load.timescale()
 eph = load('de440.bsp')
 sun = eph['Sun']
@@ -14,40 +39,22 @@ t0 = ts.now()
 t1 = t0 + 1
 
 t, y = almanac.find_risings(observer, sun, t0, t1)
+clock(t)
 # print(t.utc_iso(' '))
 
 t, y = almanac.find_settings(observer, sun, t0, t1)
+clock(t)
 # print(t.utc_iso(' '))
 
 moon = eph['Moon']
 
 t, y = almanac.find_risings(observer, moon, t0, t1)
+clock(t)
 # print('Moonrises (UTC):', t.utc_iso(' '))
 
 t, y = almanac.find_settings(observer, moon, t0, t1)#
-time = t.utc_datetime()[0]
+clock(t)
 # print(time.hour)
 # print('Moonsets (UTC):', t.utc_datetime())
-if time.hour > 12:
-    a = time.hour - 12
-else:
-    a = time.hour
 
-b =  a + time.minute/60 + time.second/3600
 
-b=3
-
-angle = -((b/12 * math.pi)*2) + math.pi/2
-
-x = math.cos(angle)
-print(x)
-y = math.sin(angle)
-
-img = plt.imread('clock.png')
-fig, ax = plt.subplots()
-ax.imshow(img, extent=[-1, 1, -1, 1])
-plt.plot(x, y, 'ro')
-plt.xlim(-1, 1)
-plt.ylim(-1, 1)
-
-plt.show()
